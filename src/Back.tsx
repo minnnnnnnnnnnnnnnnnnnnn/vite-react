9d7153a2b08c935ad45ad1da05793e77cee7dcc4
louischo303 <louischo303@gmail.com>
2026-04-01 07:15:32 +0800

    ...

--- src/Back.tsx
+++ src/Back.tsx
@@ -1,7 +1,7 @@
 
 function Back( { href , b = false } : { href : string , b ?: boolean } ) 
 {
-    return <a className="printNoDisplay" href={ href }>{ b ? "回首頁" : "回上一頁" }</a> ; 
+    return <a className="printNoDisplay" href={ href == "/" ? "" : href }>{ b ? "回首頁" : "回上一頁" }</a> ; 
 }
 
 export default Back ; 
